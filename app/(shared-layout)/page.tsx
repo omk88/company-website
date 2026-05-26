bc877a0421efd9b04ee4e31a3c18eb86d30747b5
Emmanuel Learmount <oldmanjenkins56@gmail.com>
2026-05-26 11:54:50 +0100

    View products button, logo and styling

--- app/(shared-layout)/page.tsx
+++ app/(shared-layout)/page.tsx
@@ -1,9 +1,29 @@
 import TypewriterEffect from "@/components/web/TypeWriter";
+import Link from "next/link";
+import { Button, buttonVariants } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
+import { cn } from "@/lib/utils";
+
 
 export default function Home() {
   return (
-    <div>
-      <TypewriterEffect />
+    <div className="mt-50">
+      <div className="flex flex-row items-center justify-start gap-8 flex-wrap">
+        
+        <div className="min-w-[800px] shrink-0">
+          <TypewriterEffect /> 
+        </div>
+
+        <Link 
+          className={cn(
+            buttonVariants({ variant: "outline" }), 
+            "text-xl font-bold uppercase tracking-wider px-10 py-7 h-auto border-2 gap-4 rounded-none"
+          )} 
+          href="/products"
+        > 
+          View products <ArrowRight className="size-6"/>
+        </Link>
+      </div>
     </div>
   );
 }
--- components/web/Navbar.tsx
+++ components/web/Navbar.tsx
@@ -6,15 +6,24 @@ import { Button, buttonVariants } from "../ui/button";
 import { ThemeToggle } from "./ThemeToggle";
 import { RxLinkedinLogo } from "react-icons/rx";
 import { AiOutlineInstagram } from "react-icons/ai";
+import { cn } from "@/lib/utils";
+import Image from 'next/image';
 
 
 export function Navbar() {
 
     const router = useRouter();
+    const anim = "relative no-underline hover:no-underline after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-full after:origin-bottom-right after:scale-x-0 after:bg-current after:transition-transform after:duration-300 hover:after:origin-bottom-left hover:after:scale-x-100";
 
     return (
         <nav className="w-full py-5 flex items-center justify-between">
-            <div className="flex items-center gap-8">
+            <div className="flex items-center gap-2.5">
+                <Image
+                    src={"https://i.ibb.co/JRXCHVWB/logo3.png"}
+                    alt="image"
+                    width="50"
+                    height="50"
+                />
                 <Link href="/">
                     <h1 className="text-3xl font-bold">
                         taQtiQ
@@ -22,11 +31,11 @@ export function Navbar() {
                 </Link>
 
                 <div className="flex items-center gap-10 ml-4">
-                    <Link className={buttonVariants({ variant: "link" })} href="/">Home</Link>
-                    <Link className={buttonVariants({ variant: "link" })} href="/about">About</Link>
-                    <Link className={buttonVariants({ variant: "link" })} href="/insights">Insights</Link>
-                    <Link className={buttonVariants({ variant: "link" })} href="/contact">Contact</Link>
-                    <Link className={buttonVariants({ variant: "link" })} href="/products">Products</Link>
+                    <Link className={cn(buttonVariants({ variant: "link" }), anim)} href="/">Home</Link>
+                    <Link className={cn(buttonVariants({ variant: "link" }), anim)} href="/about">About</Link>
+                    <Link className={cn(buttonVariants({ variant: "link" }), anim)} href="/insights">Insights</Link>
+                    <Link className={cn(buttonVariants({ variant: "link" }), anim)} href="/contact">Contact</Link>
+                    <Link className={cn(buttonVariants({ variant: "link" }), anim)} href="/products">Products</Link>
                 </div>
             </div>
             <div className="flex items-center gap-5 ml-4">
