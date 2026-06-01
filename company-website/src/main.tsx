7fffd1cec267959b58b4949f1e00031a9e223a0c
Emmanuel Learmount <oldmanjenkins56@gmail.com>
2026-06-02 00:05:40 +0100

    Update main.tsx

--- company-website/src/main.tsx
+++ company-website/src/main.tsx
@@ -1,3 +1,4 @@
+/// <reference types="vite/client" />
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ConvexProvider, ConvexReactClient } from "convex/react";
