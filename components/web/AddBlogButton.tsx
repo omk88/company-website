339c013e5140c7eec1409aa9f6d55776fa510ee4
Emmanuel Learmount <oldmanjenkins56@gmail.com>
2026-06-14 14:13:42 +0100

    Fetch auth query

--- components/web/AddBlogButton.tsx
+++ components/web/AddBlogButton.tsx
@@ -5,17 +5,18 @@ import { Plus } from "lucide-react";
 import { buttonVariants } from "@/components/ui/button";
 import { useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
+import { fetchAuthQuery } from "@/lib/auth-server";
 
 export function AddBlogButton() {
-    const user = useQuery(api.auth.getCurrentUser);
+    const user = fetchAuthQuery(api.auth.getCurrentUser);
     
-    const userEmail = user?.email || "";
-    const companyDomain = "@taqtiq.tech";
-    const isCompanyUser = userEmail.endsWith(companyDomain);
+    //const userEmail = user?.email || "";
+    //const companyDomain = "@taqtiq.tech";
+    //const isCompanyUser = userEmail.endsWith(companyDomain);
 
     console.log("CURRENT USER:", user);
 
-    if (!isCompanyUser) return null;
+    //if (!isCompanyUser) return null;
 
     return (
         <div className="flex justify-end mb-6">
