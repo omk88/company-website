942f48786ef6c97ecefa9e7fae7746e6eb3c7b79
Emmanuel Learmount <oldmanjenkins56@gmail.com>
2026-06-14 17:57:36 +0100

    Removed console logs for production

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,9 @@ const nextConfig: NextConfig = {
       }
     ],
   },
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production" ? { exclude: ["error"] } : false,
+  },
 };
 
 export default nextConfig;
