f1b03991494207ec17854e6097473f16a976579f
Emmanuel Learmount <oldmanjenkins56@gmail.com>
2026-07-04 18:35:05 +0100

    Removed featured blogs to test for performance

--- components/web/RightSidebar.tsx
+++ components/web/RightSidebar.tsx
@@ -34,11 +34,7 @@ export function RightSidebar() {
               <span>Featured</span>
             </h1>
           </SidebarGroupLabel>
-          <SidebarGroupContent>
-            <Suspense fallback={<FeaturedBlogsSkeleton />}>
-              <FeaturedBlogs />
-            </Suspense>
-          </SidebarGroupContent>
+
         </SidebarGroup>
         <Separator />
         <SidebarGroup>
