55ede5e7818c1c3b08c9a8f4da729f80c77f4180
Emmanuel Learmount <oldmanjenkins56@gmail.com>
2026-09-11 14:00:15 +0100

    Further improvements to about page

--- app/(shared-layout)/about/page.tsx
+++ app/(shared-layout)/about/page.tsx
@@ -39,7 +39,20 @@ export default function About() {
             TaQtiQ is a dedicated team of software engineers and IT professionals delivering innovative digital solutions. 
           </p>
 
-          <div className="w-full my-12 py-6 border-y border-neutral-200/80 dark:border-neutral-800/80 text-neutral-600 dark:text-neutral-400">
+          <div className="flex flex-col items-center w-full my-12 py-6 border-y border-neutral-200/80 dark:border-neutral-800/80 text-neutral-600 dark:text-neutral-400">
+            <h1
+              className={cn(
+                "py-6",
+                "text-2xl sm:text-4xl md:text-3xl", 
+                "font-semibold", 
+                "tracking-tight", 
+                "text-foreground",
+                "leading-tight",
+                "max-w-3xl"
+              )}
+            >
+              Redefining standards, not just following them
+            </h1>
             <p className="max-w-lg mx-auto text-center">
               Spotting gaps in current technologies and rapidly developing novel solutions. TaQtiQ combines deep systems expertise with an action oriented approach to software design. At TaQtiQ, we're not just writing code. We're architecting effective, scalable platforms and redefining problems to get technology to deliver on its promises.
             </p>
